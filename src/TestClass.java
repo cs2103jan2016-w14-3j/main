c4b67e0db99f49b87c1bb87be48b522d3c7932e8
hou-b <hou-b@172.25.100.181>
2016-03-30 11:12:20 +0800

    Added test case

---
+++ src/TestClass.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
+
+import main.java.data.PRIORITY_LEVEL;
+import main.java.data.TASK_NATURE;
+import main.java.data.TASK_STATUS;
+import main.java.data.Task;
+import main.java.logic.Logic;
+import main.java.storage.StorageController;
+
+public class TestClass {
+
+	private PrettyTimeParser parser = new PrettyTimeParser();
+	private Logic logic = new Logic();
+	private StorageController strCtrl = new StorageController();;
+
+	@Test
+	public void testAdd() throws Exception {
+		logic.handleUserCommand("add new task by 10pm #high", null);
+		logic.handleUserCommand("add another task by 8pm #medium", null);
+
+		ArrayList<Task> expectedList = new ArrayList<Task>();
+		expectedList.add(new Task("new task", parser.parse("10pm"), PRIORITY_LEVEL.HIGH, TASK_NATURE.DEADLINE, TASK_STATUS.UPCOMING));
+		expectedList.add(new Task("another task", parser.parse("8pm"), PRIORITY_LEVEL.MEDIUM, TASK_NATURE.DEADLINE, TASK_STATUS.UPCOMING));
+
+		assertEquals(expectedList.get(0).getTask(), logic.displayPending().get(0).getTask());
+		assertEquals(expectedList.get(0).getTime(), logic.displayPending().get(0).getTime());
+		assertEquals(expectedList.get(0).getPriority(), logic.displayPending().get(0).getPriority());
+
+		assertEquals(expectedList.get(1).getTask(), logic.displayPending().get(1).getTask());
+		assertEquals(expectedList.get(1).getTime(), logic.displayPending().get(1).getTime());
+		assertEquals(expectedList.get(1).getPriority(), logic.displayPending().get(1).getPriority());
+
+		strCtrl.clearPendingTasks();
+	}
+
+//	@Test
+//	public void testEdit() throws Exception {
+//		logic.handleUserCommand("add new task by 10pm #high", null);
+//		
+//		ArrayList<Task> expectedList = new ArrayList<Task>();
+//
+//		expectedList.add(new Task("new task", parser.parse("10pm"), PRIORITY_LEVEL.HIGH, TASK_NATURE.DEADLINE, TASK_STATUS.UPCOMING));
+//
+//		logic.handleUserCommand("edit new, old by 11pm", expectedList);
+//
+//		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
+//		assertEquals(expectedList.get(0).getTime(), strCtrl.displayPendingTasks().get(0).getTime());
+//		assertEquals(expectedList.get(0).getPriority(), strCtrl.displayPendingTasks().get(0).getPriority());
+//
+//		strCtrl.clearPendingTasks();
+//	}
+}
--- src/main/java/storage/StorageTest.java
+++ src/main/java/storage/StorageTest.java
@@ -1,113 +1,127 @@
-package main.java.storage;
-
-import static org.junit.Assert.*;
-import java.util.ArrayList;
-import org.junit.Test;
-
-import main.java.data.PRIORITY_LEVEL;
-import main.java.data.TASK_NATURE;
-import main.java.data.TASK_STATUS;
-import main.java.data.Task;
-
-public class StorageTest {
-
-	/*private Task task0 = new Task("do homework", "10am", PRIORITY_LEVEL.HIGH, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
-	private Task task1 = new Task("play soccer", "5pm",  PRIORITY_LEVEL.LOW, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
-	private Task task2 = new Task("eat dinner", "7pm",  PRIORITY_LEVEL.MEDIUM, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
-	private StorageController strCtrl = new StorageController();;
-
-	@Test
-	public void testAddTask() {
-		ArrayList<Task> expectedList = new ArrayList<Task>();	
-		//assert 1==1;
-
-		expectedList.add(task0);
-		strCtrl.addTask(task0);
-		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
-		assertEquals(expectedList.get(0).getTime(), strCtrl.displayPendingTasks().get(0).getTime());
-		assertEquals(expectedList.get(0).getPriority(), strCtrl.displayPendingTasks().get(0).getPriority());
-
-		expectedList.add(task1);
-		strCtrl.addTask(task1);
-		assertEquals(expectedList.get(1).getTask(), strCtrl.displayPendingTasks().get(1).getTask());
-		assertEquals(expectedList.get(1).getTime(), strCtrl.displayPendingTasks().get(1).getTime());
-		assertEquals(expectedList.get(1).getPriority(), strCtrl.displayPendingTasks().get(1).getPriority());
-
-		strCtrl.clearPendingTasks();
-	}
-
-	@Test
-	public void testEditTask() {
-		ArrayList<Task> expectedList = new ArrayList<Task>();
-
-		expectedList.add(task1);
-		strCtrl.addTask(task0);
-		strCtrl.editPendingTask(task0, task1);
-
-		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
-		assertEquals(expectedList.get(0).getTime(), strCtrl.displayPendingTasks().get(0).getTime());
-		assertEquals(expectedList.get(0).getPriority(), strCtrl.displayPendingTasks().get(0).getPriority());
-
-		strCtrl.clearPendingTasks();
-	}
-
-	@Test
-	public void testDeleteTask() {		
-
-		strCtrl.addTask(task0);
-		strCtrl.addTask(task1);
-		assertTrue(strCtrl.displayPendingTasks().size() == 2);
-
-		strCtrl.deletePendingTask(task0);
-		strCtrl.deletePendingTask(task1);
-		assertTrue(strCtrl.displayPendingTasks().size() == 0);
-	}
-
-	@Test
-	public void testSortByName() {
-		ArrayList<Task> expectedList = new ArrayList<Task>();	
-
-		expectedList.add(task0);
-		expectedList.add(task2);
-		expectedList.add(task1);	
-		strCtrl.addTask(task0);
-		strCtrl.addTask(task1);
-		strCtrl.addTask(task2);
-		strCtrl.sortPendingByTaskName();
-
-		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
-		assertEquals(expectedList.get(1).getTask(), strCtrl.displayPendingTasks().get(1).getTask());
-		assertEquals(expectedList.get(2).getTask(), strCtrl.displayPendingTasks().get(2).getTask());
-
-		strCtrl.clearPendingTasks();
-	}
-
-	@Test
-	public void testUndo() {
-
-		strCtrl.addTask(task0);
-		strCtrl.editPendingTask(task0, task1);
-		strCtrl.undo();
-
-		assertEquals(task0.getTask(), strCtrl.displayPendingTasks().get(0).getTask());
-
-		strCtrl.undo();
-		assertTrue(strCtrl.displayPendingTasks().size() == 0);
-	}
-
-	@Test
-	public void testMoveTaskToComplete() {
-
-		strCtrl.addTask(task0);
-		strCtrl.addTask(task1);
-
-		strCtrl.moveTaskToComplete(task0);
-		assertEquals(task0.getTask(), strCtrl.displayCompletedTasks().get(0).getTask());
-
-		strCtrl.moveTaskToComplete(task1);
-		assertEquals(task1.getTask(), strCtrl.displayCompletedTasks().get(1).getTask());
-
-		strCtrl.clearPendingTasks();
-		strCtrl.clearCompletedTasks();
-	}*/
-}
+//package main.java.storage;
+//
+//import static org.junit.Assert.*;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//import org.junit.Test;
+//import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
+//
+//import main.java.data.PRIORITY_LEVEL;
+//import main.java.data.TASK_NATURE;
+//import main.java.data.TASK_STATUS;
+//import main.java.data.Task;
+//import main.java.logic.Logic;
+//
+//public class StorageTest {
+//	
+//	private StorageController strCtrl = new StorageController();;
+//	private Task task0, task1, task2;
+//
+//	public void initialise() {
+//		task0 = new Task("do homework", "10am", PRIORITY_LEVEL.HIGH, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
+//		task1 = new Task("play soccer", "5pm",  PRIORITY_LEVEL.LOW, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
+//		task2 = new Task("eat dinner", "7pm",  PRIORITY_LEVEL.MEDIUM, TASK_NATURE.EVENT, TASK_STATUS.UPCOMING);
+//	}
+//		
+//	@Test
+//	public void testAddTask() throws Exception {
+//		initialise();
+//		ArrayList<Task> expectedList = new ArrayList<Task>();	
+//		
+//		expectedList.add(task0);
+//		strCtrl.addTask(task0);
+//		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
+//		assertEquals(expectedList.get(0).getTime(), strCtrl.displayPendingTasks().get(0).getTime());
+//		assertEquals(expectedList.get(0).getPriority(), strCtrl.displayPendingTasks().get(0).getPriority());
+//
+//		expectedList.add(task1);
+//		strCtrl.addTask(task1);
+//		assertEquals(expectedList.get(1).getTask(), strCtrl.displayPendingTasks().get(1).getTask());
+//		assertEquals(expectedList.get(1).getTime(), strCtrl.displayPendingTasks().get(1).getTime());
+//		assertEquals(expectedList.get(1).getPriority(), strCtrl.displayPendingTasks().get(1).getPriority());
+//
+//		strCtrl.clearPendingTasks();
+//	}
+//
+//	@Test
+//	public void testEditTask() {
+//		initialise();
+//		ArrayList<Task> expectedList = new ArrayList<Task>();
+//
+//		expectedList.add(task1);
+//		strCtrl.addTask(task0);
+//		strCtrl.editPendingTask(task0, task1);
+//
+//		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
+//		assertEquals(expectedList.get(0).getTime(), strCtrl.displayPendingTasks().get(0).getTime());
+//		assertEquals(expectedList.get(0).getPriority(), strCtrl.displayPendingTasks().get(0).getPriority());
+//
+//		strCtrl.clearPendingTasks();
+//	}
+//
+//	@Test
+//	public void testDeleteTask() {		
+//		initialise();
+//		
+//		strCtrl.addTask(task0);
+//		strCtrl.addTask(task1);
+//		assertTrue(strCtrl.displayPendingTasks().size() == 2);
+//
+//		strCtrl.deletePendingTask(task0);
+//		strCtrl.deletePendingTask(task1);
+//		assertTrue(strCtrl.displayPendingTasks().size() == 0);
+//	}
+//
+//	@Test
+//	public void testSortByName() {
+//		initialise();
+//		ArrayList<Task> expectedList = new ArrayList<Task>();	
+//
+//		expectedList.add(task0);
+//		expectedList.add(task2);
+//		expectedList.add(task1);	
+//		strCtrl.addTask(task0);
+//		strCtrl.addTask(task1);
+//		strCtrl.addTask(task2);
+//		strCtrl.sortPendingByTaskName();
+//
+//		assertEquals(expectedList.get(0).getTask(), strCtrl.displayPendingTasks().get(0).getTask());
+//		assertEquals(expectedList.get(1).getTask(), strCtrl.displayPendingTasks().get(1).getTask());
+//		assertEquals(expectedList.get(2).getTask(), strCtrl.displayPendingTasks().get(2).getTask());
+//
+//		strCtrl.clearPendingTasks();
+//	}
+//
+//	@Test
+//	public void testUndo() {
+//		initialise();
+//		
+//		strCtrl.addTask(task0);
+//		strCtrl.editPendingTask(task0, task1);
+//		strCtrl.undo();
+//
+//		assertEquals(task0.getTask(), strCtrl.displayPendingTasks().get(0).getTask());
+//
+//		strCtrl.undo();
+//		assertTrue(strCtrl.displayPendingTasks().size() == 0);
+//	}
+//
+//	@Test
+//	public void testMoveTaskToComplete() {
+//		initialise();
+//		
+//		strCtrl.addTask(task0);
+//		strCtrl.addTask(task1);
+//
+//		strCtrl.moveTaskToComplete(task0);
+//		assertEquals(task0.getTask(), strCtrl.displayCompletedTasks().get(0).getTask());
+//
+//		strCtrl.moveTaskToComplete(task1);
+//		assertEquals(task1.getTask(), strCtrl.displayCompletedTasks().get(1).getTask());
+//
+//		strCtrl.clearPendingTasks();
+//		strCtrl.clearCompletedTasks();
+//	}
+//}
